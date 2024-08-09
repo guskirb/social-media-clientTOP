73423eb742a7054d5988abd32db7277ec5120292
guskirb <anguskirby1998@gmail.com>
2024-08-09 14:17:17 +0100

    Add key to post list

--- src/components/ui/post-list/post-list.tsx
+++ src/components/ui/post-list/post-list.tsx
@@ -1,11 +1,11 @@
 import Post from "../post/post";
 
-export default function PostList({posts}) {
+export default function PostList({ posts }) {
   return (
     <div className="flex flex-col gap-2 pt-2">
-        {posts.map((post) => (
-            <Post post={post}/>
-        ))}
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
     </div>
-  )
+  );
 }
